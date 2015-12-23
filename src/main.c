e8965ed28767d92c179b9a994a7619cc71a936d2
soomtong <soomtong@gmail.com>
2015-12-23 18:14:29 +0900

    add new round image

--- src/main.c
+++ src/main.c
@@ -53,7 +53,167 @@ static void tick_handler(struct tm *t, TimeUnits units_changed) {
   }
 }
 
-static void update_light_layer(Layer *layer, GContext *ctx) {
+static void update_rect_light_layer(Layer *layer, GContext *ctx) {
+  // set metrics
+  const uint8_t digit_w = 24, digit_h = 26;
+  const uint8_t col1 = 2, col2 = 31, col3 = 60, col4 = 89, col5 = 118;
+  const uint8_t row1 = 6, row2 = 39, row3 = 72, row4 = 104, row5 = 137;
+
+  const GRect h1 = GRect(col2, row1, digit_w, digit_h);
+  const GRect h2 = GRect(col1, row2, digit_w, digit_h);
+  const GRect h3 = GRect(col4, row1, digit_w, digit_h);
+  const GRect h4 = GRect(col5, row1, digit_w, digit_h);
+  const GRect h5 = GRect(col3, row1, digit_w, digit_h * 2 + 7);
+  const GRect h6 = GRect(col2, row2, digit_w * 2 + 5, digit_h);
+  const GRect h7 = GRect(col4, row2, digit_w * 2 + 5, digit_h);
+  const GRect h8 = GRect(col1, row3, digit_w * 2 + 5, digit_h);
+  const GRect h9 = GRect(col3, row3, digit_w * 2 + 5, digit_h);
+  const GRect h10 = GRect(col1, row1, digit_w, digit_h);
+  const GRect hour_mark = GRect(col5, row3, digit_w, digit_h);
+
+  const GRect o_clock = GRect(col1, row4, digit_w * 2 + 5, digit_h);
+  const GRect m5 = GRect(col4, row5, digit_w, digit_h);
+  const GRect m10 = GRect(col5, row4, digit_w, digit_h);
+  const GRect m10_2 = GRect(col3, row5, digit_w, digit_h);
+  const GRect m20 = GRect(col3, row4, digit_w, digit_h);
+  const GRect m30 = GRect(col4, row4, digit_w, digit_h);
+  const GRect m40 = GRect(col1, row5, digit_w, digit_h);
+  const GRect m50 = GRect(col2, row5, digit_w, digit_h);
+  const GRect min_mark = GRect(col5, row5, digit_w, digit_h);
+
+  // show hours
+  switch (prev_hour) {
+    case 0:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h2], h2);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h10], h10);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 1:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h1], h1);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 2:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h2], h2);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 3:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h3], h3);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 4:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h4], h4);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 5:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h5], h5);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 6:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h6], h6);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 7:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h7], h7);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 8:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h8], h8);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 9:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h9], h9);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 10:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h10], h10);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 11:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h1], h1);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h10], h10);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    case 12:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h2], h2);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_h10], h10);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_hour_mark], hour_mark);
+      break;
+    default:
+      break;
+  }
+
+
+  // show minutes
+  switch (prev_min) {
+    case 0:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_o_clock], o_clock);
+      break;
+    case 1:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], m5);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 2:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], m10);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 3:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], m10);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], m5);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 4:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m20], m20);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], m10_2);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 5:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m20], m20);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], m10_2);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], m5);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 6:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m30], m30);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], m10);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 7:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m30], m30);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], m10);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], m5);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 8:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m40], m40);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10_2], m10_2);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 9:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m40], m40);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10_2], m10_2);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], m5);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 10:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m50], m50);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10_2], m10_2);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 11:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], m5);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m50], m50);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10_2], m10_2);
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], min_mark);
+      break;
+    case 12:
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_o_clock], o_clock);
+      break;
+    default:
+      break;
+  }
+}
+
+static void update_round_light_layer(Layer *layer, GContext *ctx) {
   // set metrics
   const uint8_t digit_w = 24, digit_h = 26;
   const uint8_t col1 = 2, col2 = 31, col3 = 60, col4 = 89, col5 = 118;
@@ -225,7 +385,11 @@ static void load_layers(Layer *root_layer) {
   layer_add_child(root_layer, bitmap_layer_get_layer(plate));
   layer_add_child(root_layer, overlay);
 
-  layer_set_update_proc(overlay, update_light_layer);
+  #if defined(PBL_RECT)
+    layer_set_update_proc(overlay, update_rect_light_layer);
+  #else
+    layer_set_update_proc(overlay, update_round_light_layer);
+  #endif
 }
 
 static void unload_layers() {
