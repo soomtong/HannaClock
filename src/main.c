4628b86eb993d6892a68a57a658ee2a0072b9d64
soomtong <soomtong@gmail.com>
2015-12-24 12:13:02 +0900

    update semi position and trim images

--- src/main.c
+++ src/main.c
@@ -33,16 +33,10 @@ static BitmapLayer *plate;
 static Layer *overlay;
 static GBitmap *bitmaps[bitmaps_length];
 
-static uint8_t prev_hour = 63, prev_min = 63;
+static uint8_t prev_hour = 63, prev_min = 63; // just 2^6 last number
 
 static void tick_handler(struct tm *t, TimeUnits units_changed) {
-//  static char s_time_buffer[16];
-//  strftime(s_time_buffer, sizeof(s_time_buffer), "%I:%M:%S", t);
-
-//  APP_LOG(APP_LOG_LEVEL_DEBUG, "Uptime: %dh %dm %ds, flag: %d", t->tm_hour, t->tm_min, t->tm_sec, units_changed);
-
-  // set clock time
-//  uint8_t now_hour = (uint8_t)(*t).tm_hour > 12 ? (uint8_t)(*t).tm_hour - 12 : (uint8_t)(*t).tm_hour;
+  // SET CLOCK TIME
   int now_hour = t->tm_hour > 12 ? t->tm_hour - 12 : t->tm_hour;  // used less memory than above
   int now_min = t->tm_min / 5;
 
@@ -218,36 +212,17 @@ static void update_rect_light_layer(Layer *layer, GContext *ctx) {
 static void update_round_light_layer(Layer *layer, GContext *ctx) {
   // set metrics
   const GSize digit = {24, 26};
-  const uint8_t col_pad = 25, row_pad = 33;
-  const uint8_t col1 = 1, col2 = col1 + col_pad + 1, col3 = col2 + col_pad - 2, col4 = col3 + col_pad + 3, col5 = col4 + col_pad, col6 = col5 + col_pad + 1, col7 = col6 + col_pad;
+  const uint8_t col_pad = 25, row_pad = 33, start_col = 81;
+  const uint8_t col1 = 1, col2 = col1 + col_pad, col3 = col2 + col_pad, col4 = col3 + col_pad + 3,
+                col5 = col4 + col_pad, col6 = col5 + col_pad + 1, col7 = col6 + col_pad;
   const int16_t row0 = 9, row1 = row0 - row_pad, row2 = row1 - row_pad, row3 = row2 - row_pad, row4 = row3 - row_pad,
-      row5 = row4 - row_pad, row6 = row5 - row_pad, row7 = row6 - row_pad, row8 = row7 - row_pad,
-      row9 = row8 - row_pad, row10 = row9 - row_pad, row11 = row10 - row_pad, row12 = row11 - row_pad;
+                row5 = row4 - row_pad, row6 = row5 - row_pad, row7 = row6 - row_pad, row8 = row7 - row_pad,
+                row9 = row8 - row_pad, row10 = row9 - row_pad, row11 = row10 - row_pad, row12 = row11 - row_pad;
 
   const GSize hour_plate = GSize(80, 524);
   const GSize min_plate = GSize(100, 524);
 
-  const GRect h1 = (GRect) {{col2, 77}, .size = digit};
-  const GRect h2 = (GRect) {{col2 + 1, 77}, .size = digit};
-  const GRect h3 = (GRect) {{col2, 77}, .size = digit};
-  const GRect h4 = (GRect) {{col2, 77}, .size = digit};
-  const GRect h5 = GRect(col3, row1, digit.w, digit.h * 2 + 7);
-  const GRect h6 = GRect(col2, row2, digit.w * 2 + 5, digit.h);
-  const GRect h7 = GRect(col4, row2, digit.w * 2 + 5, digit.h);
-  const GRect h8 = GRect(col1, row3, digit.w * 2 + 5, digit.h);
-  const GRect h9 = GRect(col3, row3, digit.w * 2 + 5, digit.h);
-  const GRect h10 = (GRect) {{col1, 77}, .size = digit};;
   const GRect hour_mark = (GRect) {{col3, 77}, .size = digit};
-
-  const GRect o_clock = GRect(col1, row4, digit.w * 2 + 5, digit.h);
-  const GRect m5 = GRect(col4, row5, digit.w, digit.h);
-  const GRect m10 = GRect(col5, row4, digit.w, digit.h);
-  const GRect m10_2 = GRect(col3, row5, digit.w, digit.h);
-  const GRect m20 = GRect(col3, row4, digit.w, digit.h);
-  const GRect m30 = GRect(col4, row4, digit.w, digit.h);
-  const GRect m40 = (GRect) {{col4, 77}, .size = digit};
-  const GRect m50 = (GRect) {{col4, 77}, .size = digit};
-  const GRect min_mark = GRect(col5, row5, digit.w, digit.h);
 
   // show hours
   switch (prev_hour) {
@@ -326,79 +301,79 @@ static void update_round_light_layer(Layer *layer, GContext *ctx) {
   // show minutes
   switch (prev_min) {
     case 0:
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row0}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row0}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_o_clock], (GRect) {.origin = {col4, 77}, .size = {(int16_t) (digit.w * 2 + 2), digit.h}});
       break;
     case 1: // 5분
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row1}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row1}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col5, 77}, digit});
       break;
     case 2:
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row2}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row2}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col5, 77}, digit});
       break;
     case 3: // 15
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row3}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row3}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], (GRect) {{col5, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col6, 77}, digit});
       break;
     case 4:
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row4}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row4}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m20], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], (GRect) {{col5, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col6, 77}, digit});
       break;
     case 5: // 25
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row5}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row5}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m20], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], (GRect) {{col5, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], (GRect) {{col6, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col7, 77}, digit});
       break;
     case 6:
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row6}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row6}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m30], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], (GRect) {{col5, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col6, 77}, digit});
       break;
     case 7: // 35
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row7}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row7}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m30], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10], (GRect) {{col5, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], (GRect) {{col6, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col7, 77}, digit});
       break;
     case 8:
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row8}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row8}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m40], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10_2], (GRect) {{col5, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col6, 77}, digit});
       break;
     case 9: //45
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row9}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row9}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m40], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10_2], (GRect) {{col5, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], (GRect) {{col6, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col7, 77}, digit});
       break;
     case 10:
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row10}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row10}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m50], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10_2], (GRect) {{col5, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col6, 77}, digit});
       break;
     case 11: // 55
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row11}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row11}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m50], (GRect) {{col4, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m10_2], (GRect) {{col5, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_m5], (GRect) {{col6, 77}, digit});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_min_mark], (GRect) {{col7, 77}, digit});
       break;
     case 12:
-      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{80, row12}, min_plate});
+      graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_plate_round_m], (GRect) {{start_col, row12}, min_plate});
       graphics_draw_bitmap_in_rect(ctx, bitmaps[bitmap_o_clock], (GRect) {.origin = {col4, 77}, .size = {(int16_t) (digit.w * 2 + 2), digit.h}});
       break;
     default:
@@ -419,7 +394,7 @@ static void load_layers(Layer *root_layer) {
   layer_add_child(root_layer, overlay);
 
   // Conditionally print out the shape of the display
-  APP_LOG(APP_LOG_LEVEL_INFO, "This is a %s display!", PBL_IF_RECT_ELSE("rectangular", "round"));
+//  APP_LOG(APP_LOG_LEVEL_INFO, "This is a %s display!", PBL_IF_RECT_ELSE("rectangular", "round"));
 
 #if defined(PBL_ROUND)
   layer_set_update_proc(overlay, update_round_light_layer);
