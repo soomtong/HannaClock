5e0d6547d84d6ac68af08fc8dddd2732ec0adf13
soomtong <soomtong@gmail.com>
2015-12-25 03:37:01 +0900

    updated link

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -3,7 +3,7 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-    var url = 'https://rawgit.com/soomtong/HannaClock/dev/config/index.html';
+    var url = 'https://rawgit.com/soomtong/HannaClock/master/config/index.html';
     //console.log('Showing configuration page: ' + url);
 
     Pebble.openURL(url);
